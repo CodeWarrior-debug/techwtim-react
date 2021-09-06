22c9098a0c674a2d4eb98b3e28534c2902f7bc8c
James Jordan <jpjordan1987@gmail.com>
2021-09-06 12:57:42 -0400

    through vid 5

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import './App.css';
-import Info from './components/Info'
+import "./App.css";
+import Info from "./components/Info";
+import { useState } from "react";
 
 function App() {
   return (
     <>
-<Info />
-<AddItem />
-</>
-
+      <Info title="Inventory" />
+      <AddItem title="goodness" text="Tim" number={2} />
+      <AddItem text="joe" />
+      <AddItem />
+      <ButtonState />
+    </>
   );
 }
 
-
-
-function AddItem(){
-  const value = "default";
-
-
+function AddItem(props) {
   return (
     <form>
       <label for="text-form">Type-something</label>
-      <input type="text" value={value} id="text-form"/>
+      <input type="text" value={props.text} id="text-form" />
+      <p>{props.number}</p>
     </form>
-  )
+  );
+}
+
+function ButtonState() {
+  const [title, setTitle] = useState("");
+  const [count, setCount] = useState(0);
+
+  const updateTitleClicked = () => {
+    setTitle("New Title") ;
+  };
+
+  const updateCounterClicked = () => {
+    setCount(count + 1) ;
+  };
+
+  function Data(props) {
+    return (
+      <div>
+        <Data title={title} count={count} />
+        <p>Title: {props.title}</p>
+        <p>Count: {props.count}</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <p>Title: {title} </p>
+      <p>Counter: {count} </p>
+      <button onClick={updateTitleClicked}>Update Title </button>
+      <button onClick={updateCounterClicked}>Update Count</button>
+    </>
+  );
 }
 
 export default App;
--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,19 +1,42 @@
 import React from "react";
 
-function Info() {
-  const title = "this is my title";
-  const showTitle = true;
 
-  if (showTitle) {
-    return (
-      <>
-        <h1>{title}</h1>
-        <p>Manage your stuff.</p>
-      </>
-    );
-  } else {
-    return <p>empty</p>;
+// Info.defaultProps={
+//   title:'Default'
+// }
+
+class Info extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      count:0,
+    };
+    this.buttonPressed =this.buttonPressed.bind(this)
   }
-}
+
+  buttonPressed(){
+    this.setState({ 
+      count:this.state.count +1,
+    })
+  }
+
+  render(props) {
+    const showTitle = true;
+    const title = this.props.title;
+
+    if (showTitle) {
+      return (
+        <>
+          <h1>{title}</h1>
+          <p>Manage your stuff.</p>
+          <p>Count: {this.state.count}</p>
+          <button onClick={this.buttonPressed} > Add +1 </button>
+        </>
+      );
+    }
+  }
+  }
 
 export default Info;
